3abc5721bb8515f786169ca0dad18ab653c0e29f
danieldemesa <danieldemesa15@gmail.com>
2025-06-30 08:59:58 -0400

    Clean up code and remove comments for simplicity

--- server/index.js
+++ server/index.js
@@ -3,26 +3,21 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-// Load env vars
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
-// Test Route
 app.get('/', (req, res) => {
   res.send('What2Wear API is running...');
 });
 
-// Connect to MongoDB and start server
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
--- server/models/User.js
+++ server/models/User.js
@@ -13,7 +13,6 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-// Encrypt password before saving
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
